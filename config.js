aba44f8ca8d708dd9a0f4c6fa1e211aeb877a547
khanhtruongloveu <88968349+khanhtruongloveu@users.noreply.github.com>
2021-08-17 23:59:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Hee luu cậu!',
+    introTitle: 'Hế nhô cậu!🙋‍♂️',
     introDesc: `Tớ có điều này muốn hỏi cậu, nhớ phải trả lời thật lòng nha`,
     btnIntro: 'oke',
     title: 'Cậu xem tớ là gì vậy 😙',
@@ -9,7 +9,7 @@ const CONFIG = {
     question:'Nói cho tới lý do vì sao tớ lại quan trọng với cậu đii🐱‍🏍🐱‍🏍🐱‍🏍 <3',
     btnReply: 'Gửi cho tớ 😁',
     reply: 'Tại vì cậu đẹp trai, hài hước nheee hihi😋 ',
-    mess: 'Tớ biết mà 🥰. Love you 300.000 😘😘',
+    mess: 'Tớ biết mà 🥰. Cậu xinh lắm lun áaaa 🙆‍♂️😘 ',
     messDesc: 'còn giờ thì chờ gì nữa mà không inbox cho tớ đi nào.❤🧡💛💙💚💜🖤💖💝',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/profile.php?id=100034444426000' //link mess của các bạn. VD: https://m.me/nam.nodemy
